89d207dbfcbd95936d615fd0756955183cf9defd
ihriyad <ihriyad.dev@gmail.com>
2026-05-30 06:35:21 +0600

    fixed proxy route

--- src/proxy.js
+++ src/proxy.js
@@ -13,5 +13,5 @@ export async function proxy(request) {
 }
 
 export const config = {
-  matcher: ["/all_pets/:path", "/dashboard/my_listing"],
+  matcher: ["/all_pets/:path", "/dashboard/add_pet"],
 };
